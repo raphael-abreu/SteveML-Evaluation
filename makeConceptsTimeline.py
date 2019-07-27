87dace90440732d027b51f9629f18aa9f8b796c6
Raphael Abreu <raphael.s.abreu@gmail.com>
2019-07-27 20:03:58 -0300

    first try

---
+++ makeConceptsTimeline.py
@@ -0,0 +1,41 @@
+import os,json
+from collections import defaultdict
+
+path = 'data/recognition/'
+
+#for filename in os.listdir(path):
+
+with open(path+'Babylonad.json') as json_file:
+    json_data = json.load(json_file)
+
+
+
+# Create a set with every concept name
+allConcepts = set()
+for frame in json_data:
+    for concept in frame['data']['concepts']:
+        if concept['name'] not in allConcepts:
+            allConcepts.add(concept['name']) 
+        #print('%s %f' % (concept['name'], concept['value']))
+print(allConcepts)
+
+
+
+# Create a dict of lists. For each concept we have a dict of the size duration of the video
+## 0 where the concept is not present and 1 where is present
+conceptDict=defaultdict(list,{k:[] for k in allConcepts})
+
+
+for key in conceptDict.keys():
+    for frame in json_data:
+        found = False
+        for concept in frame['data']['concepts']:
+            if key == concept['name']:
+                conceptDict[key].append(1)
+                found = True
+                break
+        if not found:
+            conceptDict[key].append(0)
+
+print(len(conceptDict['cold']))
+print(conceptDict['cold'])
---
+++ predictConcepts.py
@@ -0,0 +1,43 @@
+from clarifai.errors import ApiError
+from clarifai.rest import ClarifaiApp
+import json 
+
+import os
+directory = 'data/Action/'
+outDir = 'data/recognition/'
+
+app = ClarifaiApp(api_key='c6d097d9aeb64d9790b94fbb69c17ae4')
+
+m = app.public_models.general_model
+
+alreadyListedFiles = [os.path.splitext(f)[0] for f in os.listdir(outDir) if os.path.isfile(os.path.join(outDir, f))]
+
+for filename in os.listdir(directory):
+    nameToSave = os.path.splitext(filename)[0]
+    item = os.path.join(directory, filename)
+    if os.path.isfile(item) and nameToSave not in alreadyListedFiles:
+
+        try:
+            print('curr file: %s' % filename)
+            # There are also methods m.predict_by_base64 and m.predict_by_bytes
+            response = m.predict_by_filename(os.path.join(directory, filename),is_video=True,sample_ms=1000)
+        except ApiError as e:
+            print('Error status code: %d' % e.error_code)
+            print('Error description: %s' % e.error_desc)
+            if e.error_details:
+                print('Error details: %s' % e.error_details)
+            exit(1)
+
+
+        frames = response['outputs'][0]['data']['frames']
+        with open(outDir+nameToSave+'.json', 'w') as f:
+            json.dump(frames, f)
+
+        for frame in frames:
+            print('###%d###' % frame['frame_info']['time'])
+            #print('#\n')
+            #f.write('###%d###\n' % frame['frame_info']['time'])
+            for concept in frame['data']['concepts']:
+                print('%s %f' % (concept['name'], concept['value']))
+                #f.write('%s %f\n' % (concept['name'], concept['value']))
+       
